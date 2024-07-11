f5ce49fdbc88f4655339df19ea44727c40d29e62
Aaditya Srivastava <Aaditya.uwwu@gmail.com>
2024-07-11 09:18:28 +0530

    mouse callback for screen control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+import cv2
 
 # Connection to the Android device
 adb_connect = subprocess.Popen('adb connect 192.168.29.82:24', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -23,3 +24,30 @@ window_aspect_ratio = device_width / device_height
 max_window_height = 800
 window_height = max_window_height
 window_width = int(window_height * window_aspect_ratio)
+
+# Device Pointer on Pc to control mobile
+mouse_down = False
+start_x, start_y = 0, 0
+
+def mouse_callback(event, x, y, flags, param):
+    global mouse_down, start_x, start_y
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        mouse_down = True
+        start_x, start_y = x, y
+    elif event == cv2.EVENT_LBUTTONUP:
+        mouse_down = False
+
+        # Tap coordinates for PC -> mobile control
+        tap_x = int((x / window_width) * device_width)
+        tap_y = int((y / window_height) * device_height)
+
+        subprocess.Popen(f'adb shell input tap {tap_x} {tap_y}', shell=True)
+    elif event == cv2.EVENT_MOUSEMOVE and mouse_down:
+        # Swipe coordinates based on the screen resolution
+        start_x_device = int((start_x / window_width) * device_width)
+        start_y_device = int((start_y / window_height) * device_height)
+        end_x_device = int((x / window_width) * device_width)
+        end_y_device = int((y / window_height) * device_height)
+
+        subprocess.Popen(f'adb shell input swipe {start_x_device} {start_y_device} {end_x_device} {end_y_device} 500', shell=True)
